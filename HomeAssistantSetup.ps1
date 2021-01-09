9dd9c8f0e1902c0132eefc1bbef12a974dfd95c8
hugifreyr <47507738+hugifreyr@users.noreply.github.com>
2021-01-09 13:23:09 +0000

    Upgrade function added

--- HomeAssistantSetup.ps1
+++ HomeAssistantSetup.ps1
@@ -5,7 +5,7 @@
 #Docker need to be installed and if running on Windows, you need to switch the Docker environment to Linux containers.
 #The folder need to be manual created on local machine 
 #The folder or root of the folder need to be added to Docker->Setting->Resources->File Sharing 
-function New-HADockerEnv($name = "home-assistant", $volume = "C:\Docker\homeassistant", $port = "8123")
+function New-HADockerEnv($name = "home-assistant", $volume = "C:\Docker\homeassistant", $port = "8123", $SSLenabled = $false)
 {
     $portforward = $port + ":8123"
     $volume = $volume + ":/config"
@@ -17,7 +17,23 @@ function New-HADockerEnv($name = "home-assistant", $volume = "C:\Docker\homeassi
     
     #Need to sleep for 5 sec, because HA is still starting
     Start-Sleep -s 5
-    Start-Process "http://localhost:$port"
+    if($SSLenabled -eq $true)
+    {
+        Start-Process "https://localhost:$port"
+    }
+    else {
+        Start-Process "http://localhost:$port"      
+    }
+
+}
+
+function Get-UpgradeHADockerEnv {
+    docker stop "home-assistant"
+    docker rm "home-assistant"
+    docker rmi "homeassistant/home-assistant:stable"
+    docker pull homeassistant/home-assistant:stable
+
+    New-HADockerEnv -SSLenabled $true
 }
 
 #Old PFXfolder path was C:\ProgramData\Certify\certes\assets\pfx\
