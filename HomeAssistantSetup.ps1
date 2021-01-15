df75a750cfd121b88a2ffb9889bcb427e153d965
hugifreyr <47507738+hugifreyr@users.noreply.github.com>
2021-01-15 17:34:48 +0000

    wildcard support

--- HomeAssistantSetup.ps1
+++ HomeAssistantSetup.ps1
@@ -5,8 +5,7 @@
 #Docker need to be installed and if running on Windows, you need to switch the Docker environment to Linux containers.
 #The folder need to be manual created on local machine 
 #The folder or root of the folder need to be added to Docker->Setting->Resources->File Sharing 
-function New-HADockerEnv($name = "home-assistant", $volume = "C:\Docker\homeassistant", $port = "8123", $SSLenabled = $false)
-{
+function New-HADockerEnv($name = "home-assistant", $volume = "C:\Docker\homeassistant", $port = "8123", $SSLenabled = $false) {
     $portforward = $port + ":8123"
     $volume = $volume + ":/config"
     Write-Output $port
@@ -17,8 +16,7 @@ function New-HADockerEnv($name = "home-assistant", $volume = "C:\Docker\homeassi
     
     #Need to sleep for 5 sec, because HA is still starting
     Start-Sleep -s 5
-    if($SSLenabled -eq $true)
-    {
+    if ($SSLenabled -eq $true) {
         Start-Process "https://localhost:$port"
     }
     else {
@@ -34,7 +32,7 @@ function Get-UpgradeHADockerEnv( $path = "C:\Docker\homeassistant", $SSLenabled 
     docker pull homeassistant/home-assistant:stable
 
     $path = "C:\Docker\homeassistant"
-    $destinationPath = "$path-backup-" + (Get-Date).tostring(“dd.MM.yyyy”)
+    $destinationPath = "$path-backup-" + (Get-Date).tostring("dd.MM.yyyy")
     Copy-Item -Force -Recurse -Verbose -Path $path\ -Destination $destinationPath -PassThru 
 
     New-HADockerEnv -SSLenabled $SSLenabled 
@@ -42,26 +40,35 @@ function Get-UpgradeHADockerEnv( $path = "C:\Docker\homeassistant", $SSLenabled 
 
 #Old PFXfolder path was C:\ProgramData\Certify\certes\assets\pfx\
 #New PFXfolder parh is "C:\ProgramData\Certify\assets\domainname\"
-function Get-HAConvertPFX2PEM ( $PFXfolder = "C:\ProgramData\Certify\assets\", $HAfolder = "C:\Docker\homeassistant\", $OpenSSLfolder = "C:\Program Files\Git\mingw64\bin\" , $Domain  )
-{
+function Get-HAConvertPFX2PEM {
+    Param(
+        $PFXfolder = "C:\ProgramData\Certify\assets\",
+        $HAfolder = "C:\Docker\homeassistant\",
+        $OpenSSLfolder = "C:\Program Files\Git\mingw64\bin\",
+        [parameter(Mandatory = $true)]
+        $Domain,
+        $Wildcard = $false 
+    )
+
+    if ($Wildcard -eq $true) {
+        $PFXfolder = $PFXfolder + "_."
+    }
+
     $PFXfolder = $PFXfolder + $Domain + "" 
     Write-Output $PFXfolder
     $temp = Get-ChildItem -Path $PFXfolder -Recurse -include *.pfx | Select-Object -first 100 | Sort-Object LastWriteTime -Descending
     Write-Output $temp
 
-    foreach ( $tmp in $temp )
-    {
+    foreach ( $tmp in $temp ) {
         $name = $tmp.Name
         $file = "$PFXfolder\$name"
         Write-Output $file
 
-        $Domain = "CN=" + $Domain
-        try 
-        {
+        try {
             $tempPFX = Get-PfxCertificate -FilePath $file
+            Write-Output $tempPFX.Subject
 
-            if( $tempPFX.Subject -ieq $Domain)
-            {
+            if ( $tempPFX.Subject -ieq ( "CN=" + $Domain ) -or $tempPFX.Subject -ieq ("CN=*." + $Domain) ) {
                 Write-Output $name
                 Set-Location $OpenSSLfolder
 
